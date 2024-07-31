3f480c3ca5a083bf37c1992588ca1260215387b2
korraaravind <113603029+korraaravind@users.noreply.github.com>
2024-07-31 10:18:00 +0530

    {}

---
+++ Function.py
@@ -0,0 +1,33 @@
+# function with no arguments
+from unittest import result
+
+
+# def greet():
+#     print("hello well come to python function")
+#
+# def add_sub (x,y):
+#     C=x+y
+#     D=x-y
+#     greet()
+#     print( C, D)
+#
+# add_sub(10,20)
+
+# Multiplication
+# def multi(a,b):
+#     result=a*b
+#     return  result
+#     print(sum_result)
+#     sum_result=multi(10,20)
+
+# Addition of tow number
+
+def add_num(x,y):
+    result = x+y
+    return  result
+sum_result = add_num(10,20)
+print(sum_result)
+
+# function with parameter and without return type
+
+
